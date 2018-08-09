8851381affd4118a40f6a5e4d8cc2aba5ad8f969
Kajornjit <john@iglu.net>
2018-08-09 15:00:59 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+function logMember(target, name, descriptor) {
+    const original = descriptor.value;
+    descriptor.value = function (...args) {
+        console.log('Arguments', args, ' were passed in this function');
+        const result = original.apply(this, args);
+        console.log('The result of the function is ', result);
+        return result;
+    };
+
+    return descriptor;
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'public-angular6';
+    title = 'Pluto Solutions';
+
+    constructor() {
+        console.log('Start app with a Simple Method ' + this.aSimpleMethod(5, 6));
+    }
+
+    @logMember
+    aSimpleMethod(a, b) {
+        return a * b;
+    }
 }
